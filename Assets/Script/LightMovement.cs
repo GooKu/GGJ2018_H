698eb8f50321804f00399865ff6403d7a7f21b3d
Michael Li <li.michael1999@gmail.com>
2018-01-27 10:10:12 +0800

    increase LightMovement.

---
+++ Assets/Script/LightMovement.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightMovement : MonoBehaviour {
+
+    public Rigidbody2D rb;
+
+    private Vector2 normal;
+    private float IncidenceAngle;
+    private float NormalAngle;
+    private float angle;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Vector2 direction;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        normal = collision.contacts[0].normal;
+
+        if (collision.gameObject.CompareTag("Mirror"))
+        {
+            normal = collision.contacts[0].normal;
+            direction = Vector2.Reflect(direction, normal);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        transform.Translate(direction * speed);
+	}
+}
