521997d5fbab1e065265e8eaa3b6483ce9161ee6
Michael Li <li.michael1999@gmail.com>
2018-01-27 15:25:57 +0800

    Added Light Source prefab and script

---
+++ Assets/Script/Source.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Source : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject light;
+	void Start()
+    {
+        light = Instantiate(light, transform.position, transform.rotation);
+        Debug.Log(transform.right);
+        light.GetComponent<LightMovement>().direction = transform.right.normalized;
+    }
+}
