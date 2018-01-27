9d6b280cfeefa7e015f01c59bd690b5ad1f09d7e
H0WT0G0 <a451201@gmail.com>
2018-01-27 17:30:43 +0800

    UI Script Update

---
+++ Assets/Script/UI/BtnScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//道具按鈕程式碼
+
+public class BtnScript : MonoBehaviour {
+
+    //被拖曳時複製的物件
+    public GameObject copyItem;
+
+    //有東西表示拖曳中
+    private Transform dragItem = null;
+
+
+    // Use this for initialization
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(dragItem != null) {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = 0;
+            dragItem.position = pos;
+        }
+    }
+
+    //拖曳時執行程式碼
+    public void OnStartDrag() {
+        //複製物件
+        dragItem = Instantiate(copyItem,Vector3.zero,Quaternion.identity).transform; 
+    }
+
+    //放開時執行程式碼
+    public void OnEndDrag() {
+        //丟下物件
+        dragItem = null;
+    }
+    
+
+}
---
+++ Assets/Script/UI/ItemBarUI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+//道具class
+[System.Serializable]
+public class ItemInfo {
+    [Tooltip("放上Item的UI")]
+    public GameObject icon;
+    public int number;
+}
+
+//道具欄位的UI
+
+public class ItemBarUI : MonoBehaviour {
+    //父物件
+    public Transform itemRoot;
+
+    //道具表單
+    [Tooltip("道具表單")]
+    public ItemInfo[] itemInfo = new ItemInfo[4];
+
+    
+
+    // Use this for initialization
+    void Start() {
+        AddItem();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+
+    }
+
+    //新增按鈕
+    public void AddItem() {
+        //複製按鈕
+        for(int i = 0; i < itemInfo.Length; i++) {
+            GameObject item = Instantiate(itemInfo[i].icon,Vector3.zero,Quaternion.identity,itemRoot) as GameObject;
+            item.transform.parent = itemRoot;
+        }
+    }
+
+}
