c4e489a33e741b9e7163f8262961de79fdcbbf84
H0WT0G0 <a451201@gmail.com>
2018-01-28 08:50:01 +0800

    Game Camera Controller

---
+++ Assets/Script/UI/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    //拉條最右
+    public float maxRight;
+
+    //攝影機最左
+    public Vector3 leftCameraPos;
+    public Vector3 rightCameraPos;
+
+    //當前狀態
+    public float cameraProcess = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // 
+}
