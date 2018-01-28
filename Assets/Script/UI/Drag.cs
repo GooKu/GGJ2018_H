7c3d2eab4f7f1312e022d98867b9f7e4600476cf
hubert322 <hubert322@gmail.com>
2018-01-28 13:23:53 +0800

    UI modifier repositioned

--- Assets/Script/UI/Drag.cs
+++ Assets/Script/UI/Drag.cs
@@ -10,12 +10,22 @@ public class Drag : MonoBehaviour
 	public Slider force;
 	public Button deselect;
 	public GameObject modifier;
+	public GameObject scrollView;
+	public GameObject nextItem;
+	public GameObject preItem;
 
 	private GameObject gb;
 	private Slider f;
 	private Slider s;
 	private Slider r;
 	private Button d;
+
+	private void Awake ()
+	{
+		scrollView = GameObject.Find ("Scroll View");
+		nextItem = GameObject.Find ("NextItem");
+		preItem = GameObject.Find ("PreItem");
+	}
 
 	private void OnMouseDrag ()
 	{
@@ -27,42 +37,37 @@ public class Drag : MonoBehaviour
 	private void OnMouseDown ()
 	{
 		if (GameObject.Find ("Modifier"))
-		{
-			/*if (GameObject.Find ("Modifier").activeSelf == true)
-				return;
-			else
-				GameObject.Find ("Modifier").SetActive (true);*/
 			return;
-		}
 			
-
 		if (gameObject.tag == "Mirror")
 		{
 			if (gb == null)
-			{
-				gb = Instantiate (modifier, GameObject.Find ("UI").transform);
-				gb.name = "Modifier";
-			}
+				spawnModifier ();
+			else return;
+
+			activate (false);
 
 			s = Instantiate (scale, GameObject.Find ("Modifier").transform);
 			s.GetComponent<Modify> ().gb = gameObject;
+			s.transform.localPosition = new Vector3 (-372, 15, 0);
 			s.name = "Scale";
 
 			r = Instantiate (rotate, GameObject.Find ("Modifier").transform);
 			r.GetComponent<Modify> ().gb = gameObject;
+			r.transform.localPosition = new Vector3 (-128, 15, 0);
 			r.name = "Rotate";
 
 			d = Instantiate (deselect, GameObject.Find ("Modifier").transform);
 			d.name = "Deselect";
+			d.transform.localPosition = new Vector3 (0, 50, 0);
 		}
 
 		else if (gameObject.tag == "Black Hole")
 		{
 			if (gb == null)
-			{
-				gb = Instantiate (modifier, GameObject.Find ("UI").transform);
-				gb.name = "Modifier";
-			}
+				spawnModifier ();
+			else
+				return;
 
 			f = Instantiate (force, GameObject.Find ("Modifier").transform);
 			f.GetComponent<Modify> ().gb = gameObject;
@@ -70,6 +75,32 @@ public class Drag : MonoBehaviour
 
 			d = Instantiate (deselect, GameObject.Find ("Modifier").transform);
 			d.name = "Deselect";
+			gameObject.GetComponent<CircleCollider2D> ().enabled = false;
 		}
+	}
+
+	private void Update ()
+	{
+		if (gb==null&& gameObject.tag == "Black Hole")
+		{
+			gameObject.GetComponent<CircleCollider2D> ().enabled = true;
+		}
+
+		if (GameObject.Find ("Deselect") && GameObject.Find ("Deselect").GetComponent<Deselect> ().canActivate)
+			activate (true);
+	}
+
+	private void spawnModifier ()
+	{
+		gb = Instantiate (modifier, GameObject.Find ("UI").transform);
+		gb.transform.localPosition = new Vector3 (0, -330, 0);
+		gb.name = "Modifier";
+	}
+
+	public void activate (bool boo)
+	{
+		scrollView.SetActive (boo);
+		nextItem.SetActive (boo);
+		preItem.SetActive (boo);
 	}
 }
